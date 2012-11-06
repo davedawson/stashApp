1967f76d79308ae1a783b6b16338d1e38df8d469
davedawson <dave@iamdavedawson.com>
2012-11-05 22:56:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StashApp::Application.config.secret_token = 'c9714659fce2976cc22b6d4fac747c652b8aa3c893abb5c9c342fc9acdf48b087ddeb98378deaf2bfa12e5d496917c42ce0342b135b2b6f882712a8f2f322448'
